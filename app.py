c8e2a61738e74d8f1bc85fda9c469d77dd1ecefd
Aniket Rege <aniketrg7@gmail.com>
2016-03-10 00:44:42 +0530

    invalid port

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def hello(name):
 	return render_template('page.html', name=name)
 
 if __name__ == '__main__':
-	app.run(debug=True, host= '0.0.0.0', port=int("500"))
+	app.run(debug=True, host= '0.0.0.0', port=int("8000"))
