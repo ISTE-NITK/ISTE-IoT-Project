844536e09383dba301c37ac5c04cabe2b5906b5d
Aniket Rege <aniketrg7@gmail.com>
2016-03-16 04:24:29 +0530

    added support for server login page, implementing post operation! Yay finally

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request 
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)
 
@@ -10,12 +10,23 @@ def index():
 def about():
     return 'A Project by ISTE- NITK Chapter.'
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login/')											
 def login():
+	return render_template('my-form.html')
+
+@app.route('/login/', methods=['POST'])
+def login_post():
+
+	text = request.form['text']
+	processed_text=text.upper()
+	return processed_text
+
+	'''
     if request.method == 'POST':
     	return 'You are using POST'
     else:
     	return 'Probably GET'
+    '''
 
 @app.route('/profile/<name>/')							#name is the variable we pass to this route 
 def hello(name):
@@ -23,3 +34,21 @@ def hello(name):
 
 if __name__ == '__main__':
 	app.run(debug=True, host= '0.0.0.0', port=int("8000"))
+
+
+
+	'''
+	@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
+    '''
